7a7bdff6ae8965241d5bdd0358e484b2531520c5
rakam nagar <rakamsingh1711@gmail.com>
2023-09-06 11:09:09 +0530

    completed

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT =5555 || process.env.PORT
+
+export const mongoDBURL = 'mongodb+srv://rakamsingh1711:NNVyqJXA7FoH7p1X@cluster0.ykxwo9f.mongodb.net/books-collection?retryWrites=true&w=majority'|| 'mongodb://0.0.0.0:27017/book'
+
--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,33 @@
-const express = require("express");
-const {PORT , mongoDBURL} = require("./configs");
-const mongoose  = require('mongoose');
-const booksRoutes = require('./routes/booksRoutes');
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
-app.use(express.json());
-app.use('/v1/books',booksRoutes);
 
-app.get('/',(req,res)=>{
-    res.status(274).send("welcome to the book collection");
+
+app.use(express.json());
+
+
+app.use(cors());
+
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
+app.use('/books', booksRoute);
 
-
-mongoose.connect(mongoDBURL)
-.then(()=>console.log("database connection established"))
-.catch((error)=>{
-    console.log(error.message);
-})
-
-app.listen(PORT,()=>
-console.log(`server started at port: ${PORT}`));
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
